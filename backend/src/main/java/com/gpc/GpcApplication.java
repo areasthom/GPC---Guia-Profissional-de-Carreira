1b02641fd9e3f3624232a191b0dcc604b934ff28
Thomas Samuel <thomluz.areas@gmail.com>
2026-09-19 19:13:55 -0400

    feat: adiciona estrutura inicial do backend Spring Boot

---
+++ backend/src/main/java/com/gpc/GpcApplication.java
@@ -0,0 +1,13 @@
+package com.gpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpcApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gpc/GpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
